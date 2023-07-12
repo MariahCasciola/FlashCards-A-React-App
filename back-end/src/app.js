113c90e71b9ac31dcb53464c060fe9e3cd741695
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-07-12 00:37:49 -0500

    set up node, express, and knex configuration, ready to create a postgres instance, migrations, and seeds

---
+++ back-end/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+// TODO: require routers and assign them to
+
+const app = express();
+
+// TODO: app.use("/", router)
+
+// TODO: not found handler
+
+// TODO: error handler
+
+module.exports = app;
